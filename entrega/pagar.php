95dee54323c24b91443172a55cfedab36c69675f
pelos6 <javieriranzo@hotmail.com>
2015-04-14 20:24:37 +0200

    commit sin entrega

--- entrega/pagar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Recuperamos la información de la sesión
-    session_start();
-    unset($_SESSION['cesta']);
-    
-    die("Gracias por su compra.<br />Quiere <a href='productos.php'>comenzar de nuevo</a>?");
-?>
